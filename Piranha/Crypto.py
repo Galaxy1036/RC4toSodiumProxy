7a8c1669a4398c12686972036155375447f03aae
Galaxy1036 <simon.hamelin6500@gmail.com>
2017-12-31 14:40:38 +0100

    Add files via upload

---
+++ Piranha/Crypto.py
@@ -0,0 +1,70 @@
+# -*- coding: utf-8 -*-
+import nacl.utils
+from nacl.public import Box, PrivateKey, PublicKey
+from Piranha.Nonce import Nonce
+
+
+class Crypto:
+
+    def __init__(self, serverKey):
+        self.serverKey    = bytes.fromhex(serverKey)  # server publicKey (can be found in libg.so)
+        self.pk           = None  # publicKey
+        self.sk           = None  # privateKey
+        self.sessionKey   = None  # sessionKey from 20100
+        self.sharedKey    = None  # sharedKey used to decrypt packet
+        self.Nonce        = None  # nonce
+        self.decryptNonce = None  # rnonce
+        self.encryptNonce = None  # snonce
+
+    def encrypt(self, packetID, payload):
+        if packetID == 10100:
+
+            return payload
+
+        elif packetID == 10101:
+
+            self.encryptNonce = Nonce()
+            self.Nonce        = Nonce(clientKey=self.pk, serverKey=self.serverKey)
+            self.sharedKey    = Box(self.sk, PublicKey(self.serverKey))
+
+            return bytes(self.pk) + self.afternm(self.sessionKey + bytes(self.encryptNonce) + payload, self.Nonce)
+
+        else:
+            return self.afternm(payload)
+
+    def decrypt(self, packetID, payload):
+        if packetID == 20100:
+            self.sessionKey = payload[4:]
+            self.sk         = PrivateKey.generate()
+            self.pk         = self.sk.public_key
+
+            return payload
+
+        elif packetID == 20103 and not self.sessionKey:
+            return payload
+
+        elif packetID == 22280 or (packetID == 20103 and self.sessionKey):
+            nonce                = Nonce(self.encryptNonce, self.pk, self.serverKey)
+            decrypted            = self.decryptnm(payload, nonce)
+            self.sharedKey       = Box.decode(decrypted[24:56])  # Overwrite the previous sharedKey
+            self.decryptNonce    = Nonce(decrypted[:24])
+
+            return decrypted[56:]
+
+        else:
+            return self.decryptnm(payload)
+
+    def afternm(self, payload, nonce=None):
+        if not nonce:
+            self.encryptNonce.increment()
+            nonce = self.encryptNonce
+
+        return self.sharedKey.encrypt(payload, bytes(nonce))[24:]
+        # 24 because the lib add 24 null bytes at the start of the payload
+
+    def decryptnm(self, payload, nonce=None):
+        if not nonce:
+            self.decryptNonce.increment()
+            nonce = self.decryptNonce
+
+        return self.sharedKey.decrypt(payload, bytes(nonce))
--- Piranha/Nonce.py
+++ Piranha/Nonce.py
@@ -1,1 +1,33 @@
+# -*- coding: utf-8 -*-
+from pyblake2 import blake2b
+from nacl.public import Box
+import nacl.utils
 
+
+class Nonce:
+
+    def __init__(self, nonce=None, clientKey=None, serverKey=None):
+        if not clientKey:
+            if nonce:
+                self._nonce = nonce
+
+            else:
+                self._nonce = nacl.utils.random(Box.NONCE_SIZE)
+
+        else:
+            b2 = blake2b(digest_size=24)
+            if nonce:
+                b2.update(bytes(nonce))
+            b2.update(bytes(clientKey))
+            b2.update(serverKey)
+            self._nonce = b2.digest()
+
+    def __bytes__(self):
+        return self._nonce
+
+    def __len__(self):
+        return len(self._nonce)
+
+    def increment(self):
+        self._nonce = (int.from_bytes(self._nonce, 'little') + 2).to_bytes(
+            Box.NONCE_SIZE, 'little')
